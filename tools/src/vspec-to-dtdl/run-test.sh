d4dcf120122fee6131a6b0ed20c453ba19815f6a
jbeman <jbeman@microsoft.com>
2020-06-29 23:20:37 +0100

    updates to fix thirdpart problems

--- tools/src/vspec-to-dtdl/run-test.sh
+++ tools/src/vspec-to-dtdl/run-test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-./vspec-to-dtdl /home/ashbeitz/repos/Vehicle-DataModel-Tools/third-party/vehicle_signal_specification/src/vehicle_signal_specification/spec/VehicleSignalSpecification.vspec
+./vspec-to-dtdl ../../third-party/vehicle_signal_specification/src/vehicle_signal_specification/spec/VehicleSignalSpecification.vspec
